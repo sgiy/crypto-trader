d61709290a2a4a2e4629842c1698767a512f60c9
sgiy <44041935+sgiy@users.noreply.github.com>
2018-10-15 23:31:04 -0400

    Adding rolling sums

--- python3/main.py
+++ python3/main.py
@@ -15,16 +15,32 @@ def test():
     for cell_index in range(2 * table_rows_one_direction):
         tableWidget.setItem(cell_index,0, QtGui.QTableWidgetItem(""))
         tableWidget.setItem(cell_index,1, QtGui.QTableWidgetItem(""))
+    sum_bid = 0
+    sum_bid_base = 0
     for bid in results['Bid']:
         tableWidget.setItem(table_rows_one_direction + bid, 0, QtGui.QTableWidgetItem("{0:.8f}".format(results['Bid'][bid]['Price'])))
         tableWidget.setItem(table_rows_one_direction + bid, 1, QtGui.QTableWidgetItem("{0:.8f}".format(results['Bid'][bid]['Quantity'])))
+        sum_bid += results['Bid'][bid]['Quantity']
+        sum_bid_base += results['Bid'][bid]['Quantity'] * results['Bid'][bid]['Price']
+        tableWidget.setItem(table_rows_one_direction + bid, 2, QtGui.QTableWidgetItem("{0:.8f}".format(sum_bid)))
+        tableWidget.setItem(table_rows_one_direction + bid, 3, QtGui.QTableWidgetItem("{0:.8f}".format(sum_bid_base)))
         tableWidget.item(table_rows_one_direction + bid, 0).setBackground(QtGui.QColor(40,167,69))
         tableWidget.item(table_rows_one_direction + bid, 1).setBackground(QtGui.QColor(40,167,69))
+        tableWidget.item(table_rows_one_direction + bid, 2).setBackground(QtGui.QColor(40,167,69))
+        tableWidget.item(table_rows_one_direction + bid, 3).setBackground(QtGui.QColor(40,167,69))
+    sum_ask = 0
+    sum_ask_base = 0
     for ask in results['Ask']:
         tableWidget.setItem(table_rows_one_direction - 1 - ask, 0, QtGui.QTableWidgetItem("{0:.8f}".format(results['Ask'][ask]['Price'])))
         tableWidget.setItem(table_rows_one_direction - 1 - ask, 1, QtGui.QTableWidgetItem("{0:.8f}".format(results['Ask'][ask]['Quantity'])))
+        sum_ask += results['Ask'][ask]['Quantity']
+        sum_ask_base += results['Ask'][ask]['Quantity'] * results['Ask'][ask]['Price']
+        tableWidget.setItem(table_rows_one_direction - 1 - ask, 2, QtGui.QTableWidgetItem("{0:.8f}".format(sum_ask)))
+        tableWidget.setItem(table_rows_one_direction - 1 - ask, 3, QtGui.QTableWidgetItem("{0:.8f}".format(sum_ask_base)))
         tableWidget.item(table_rows_one_direction - 1 - ask, 0).setBackground(QtGui.QColor(220,53,69))
         tableWidget.item(table_rows_one_direction - 1 - ask, 1).setBackground(QtGui.QColor(220,53,69))
+        tableWidget.item(table_rows_one_direction - 1 - ask, 2).setBackground(QtGui.QColor(220,53,69))
+        tableWidget.item(table_rows_one_direction - 1 - ask, 3).setBackground(QtGui.QColor(220,53,69))
 
 ## Create some widgets to be placed inside
 btn = QtGui.QPushButton('press me')
@@ -33,7 +49,7 @@ btn.clicked.connect(test)
 table_rows_one_direction = 5
 tableWidget = QtGui.QTableWidget()
 tableWidget.setRowCount(2 * table_rows_one_direction)
-tableWidget.setColumnCount(2)
+tableWidget.setColumnCount(4)
 test()
 
 plot = pg.PlotWidget()
